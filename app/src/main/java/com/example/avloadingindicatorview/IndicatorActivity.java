b090e5e00eed54153bfe0248df95e111dffbf4f2
Mai Trung Duc <maitrungduc1410@gmail.com>
2023-07-22 11:14:25 +0800

    feat: first commit

---
+++ app/src/main/java/com/example/avloadingindicatorview/IndicatorActivity.java
@@ -0,0 +1,38 @@
+package com.example.avloadingindicatorview;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.wang.avi.AVLoadingIndicatorView;
+
+/**
+ * Created by Jack Wang on 2016/8/5.
+ */
+
+public class IndicatorActivity extends AppCompatActivity {
+
+    private AVLoadingIndicatorView avi;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indicator);
+
+        String indicator=getIntent().getStringExtra("indicator");
+        avi= (AVLoadingIndicatorView) findViewById(R.id.avi);
+        avi.setIndicator(indicator);
+    }
+
+    public void hideClick(View view) {
+        avi.hide();
+        // or avi.smoothToHide();
+    }
+
+    public void showClick(View view) {
+        avi.show();
+        // or avi.smoothToShow();
+    }
+}
---
+++ app/src/main/java/com/example/avloadingindicatorview/SampleActivity.java
@@ -0,0 +1,106 @@
+package com.example.avloadingindicatorview;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.wang.avi.AVLoadingIndicatorView;
+
+/**
+ * Created by Jack Wang on 2016/8/5.
+ */
+
+public class SampleActivity extends AppCompatActivity {
+
+    private RecyclerView mRecycler;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+
+        mRecycler= (RecyclerView) findViewById(R.id.recycler);
+
+        GridLayoutManager layoutManager=new GridLayoutManager(this,4);
+        mRecycler.setLayoutManager(layoutManager);
+        mRecycler.setAdapter(new RecyclerView.Adapter<IndicatorHolder>() {
+            @Override
+            public IndicatorHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View itemView=getLayoutInflater().inflate(R.layout.item_indicator,parent,false);
+                return new IndicatorHolder(itemView);
+            }
+
+            @Override
+            public void onBindViewHolder(IndicatorHolder holder, @SuppressLint("RecyclerView") final int position) {
+                holder.indicatorView.setIndicator(INDICATORS[position]);
+                holder.itemLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent=new Intent(SampleActivity.this,IndicatorActivity.class);
+                        intent.putExtra("indicator",INDICATORS[position]);
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @Override
+            public int getItemCount() {
+                return INDICATORS.length;
+            }
+        });
+    }
+
+    final static class IndicatorHolder extends RecyclerView.ViewHolder{
+
+        public AVLoadingIndicatorView indicatorView;
+        public View itemLayout;
+
+        public IndicatorHolder(View itemView) {
+            super(itemView);
+            itemLayout= itemView.findViewById(R.id.itemLayout);
+            indicatorView= (AVLoadingIndicatorView) itemView.findViewById(R.id.indicator);
+        }
+    }
+
+
+
+    private static final String[] INDICATORS=new String[]{
+            "BallPulseIndicator",
+            "BallGridPulseIndicator",
+            "BallClipRotateIndicator",
+            "BallClipRotatePulseIndicator",
+            "SquareSpinIndicator",
+            "BallClipRotateMultipleIndicator",
+            "BallPulseRiseIndicator",
+            "BallRotateIndicator",
+            "CubeTransitionIndicator",
+            "BallZigZagIndicator",
+            "BallZigZagDeflectIndicator",
+            "BallTrianglePathIndicator",
+            "BallScaleIndicator",
+            "LineScaleIndicator",
+            "LineScalePartyIndicator",
+            "BallScaleMultipleIndicator",
+            "BallPulseSyncIndicator",
+            "BallBeatIndicator",
+            "LineScalePulseOutIndicator",
+            "LineScalePulseOutRapidIndicator",
+            "BallScaleRippleIndicator",
+            "BallScaleRippleMultipleIndicator",
+            "BallSpinFadeLoaderIndicator",
+            "LineSpinFadeLoaderIndicator",
+            "TriangleSkewSpinIndicator",
+            "PacmanIndicator",
+            "BallGridBeatIndicator",
+            "SemiCircleSpinIndicator",
+            "com.wang.avi.sample.MyCustomIndicator"
+    };
+
+}
